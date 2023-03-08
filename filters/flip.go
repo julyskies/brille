1f3011ea026a55a39fbdfe15a2289c1a56e8eb0d
Peter <p.dyumin@dunice.net>
2023-03-08 11:56:01 +0300

    Update image flipping

--- filters/flip.go
+++ filters/flip.go
@@ -2,6 +2,7 @@ package filters
 
 import (
 	"io"
+	"sync"
 
 	"github.com/julyskies/brille/v2/constants"
 	"github.com/julyskies/brille/v2/utilities"
@@ -24,23 +25,37 @@ func Flip(file io.Reader, direction string) (io.Reader, string, error) {
 	if height%2 != 0 {
 		heightCorrection = 1
 	}
-	for i := 0; i < len(img.Pix); i += 4 {
-		x, y := utilities.GetCoordinates(i/4, width)
-		var j int
-		skip := true
-		if direction == constants.FLIP_DIRECTION_HORIZONTAL && x < width/2+widthCorrection {
-			j = utilities.GetPixel(width-x-1, y, width)
-			skip = false
-		}
-		if direction == constants.FLIP_DIRECTION_VERTICAL && y < height/2+heightCorrection {
-			j = utilities.GetPixel(x, height-y-1, width)
-			skip = false
-		}
-		if !skip {
-			r, g, b := img.Pix[i], img.Pix[i+1], img.Pix[i+2]
-			img.Pix[i], img.Pix[i+1], img.Pix[i+2] = img.Pix[j], img.Pix[j+1], img.Pix[j+2]
-			img.Pix[j], img.Pix[j+1], img.Pix[j+2] = r, g, b
+	pixLen := len(img.Pix)
+	threads := utilities.GetThreads()
+	pixPerThread := utilities.GetPixPerThread(pixLen, threads)
+	var wg sync.WaitGroup
+	processing := func(thread int) {
+		defer wg.Done()
+		startIndex := pixPerThread * thread
+		endIndex := utilities.ClampMax(startIndex+pixPerThread, pixLen)
+		for i := startIndex; i < endIndex; i += 4 {
+			x, y := utilities.GetCoordinates(i/4, width)
+			var j int
+			skip := true
+			if direction == constants.FLIP_DIRECTION_HORIZONTAL && x < width/2+widthCorrection {
+				j = utilities.GetPixel(width-x-1, y, width)
+				skip = false
+			}
+			if direction == constants.FLIP_DIRECTION_VERTICAL && y < height/2+heightCorrection {
+				j = utilities.GetPixel(x, height-y-1, width)
+				skip = false
+			}
+			if !skip {
+				r, g, b := img.Pix[i], img.Pix[i+1], img.Pix[i+2]
+				img.Pix[i], img.Pix[i+1], img.Pix[i+2] = img.Pix[j], img.Pix[j+1], img.Pix[j+2]
+				img.Pix[j], img.Pix[j+1], img.Pix[j+2] = r, g, b
+			}
 		}
 	}
+	for t := 0; t < threads; t += 1 {
+		wg.Add(1)
+		go processing(t)
+	}
+	wg.Wait()
 	return utilities.EncodeResult(img, format)
 }
