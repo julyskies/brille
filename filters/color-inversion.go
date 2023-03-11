2ef893ec83011b55c7821fea79c6e74414b9d045
Peter <p.dyumin@dunice.net>
2023-03-11 17:44:33 +0300

    Update Color inversion filter

--- filters/color-inversion.go
+++ filters/color-inversion.go
@@ -2,6 +2,7 @@ package filters
 
 import (
 	"io"
+	"sync"
 
 	"github.com/julyskies/brille/v2/utilities"
 )
@@ -11,10 +12,24 @@ func ColorInversion(file io.Reader) (io.Reader, string, error) {
 	if convertationError != nil {
 		return nil, "", convertationError
 	}
-	for i := 0; i < len(img.Pix); i += 4 {
-		img.Pix[i] = 255 - img.Pix[i]
-		img.Pix[i+1] = 255 - img.Pix[i+1]
-		img.Pix[i+2] = 255 - img.Pix[i+2]
+	pixLen := len(img.Pix)
+	threads := utilities.GetThreads()
+	pixPerThread := utilities.GetPixPerThread(pixLen, threads)
+	var wg sync.WaitGroup
+	processing := func(thread int) {
+		defer wg.Done()
+		startIndex := pixPerThread * thread
+		endIndex := utilities.ClampMax(startIndex+pixPerThread, pixLen)
+		for i := startIndex; i < endIndex; i += 4 {
+			img.Pix[i] = 255 - img.Pix[i]
+			img.Pix[i+1] = 255 - img.Pix[i+1]
+			img.Pix[i+2] = 255 - img.Pix[i+2]
+		}
 	}
+	for t := 0; t < threads; t += 1 {
+		wg.Add(1)
+		go processing(t)
+	}
+	wg.Wait()
 	return utilities.EncodeResult(img, format)
 }
